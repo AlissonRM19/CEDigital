5b4af96fd0a0aaefe66c1b0f6f7ad4a2309334cd
JoSePhPM23 <128115332+JoSePhPM23@users.noreply.github.com>
2025-05-22 21:59:47 -0600

    Delete APIMongo/CEDigitalMongo.Api directory

--- APIMongo/CEDigitalMongo.Api/CEDigitalMongo.Api/Scripts/create-collections.js
+++
@@ -1,8 +0,0 @@
-﻿// CONEXIÓN CEDigital
-use CEDigital;
-
-// Colección estudiantes (M1)
-db.createCollection("estudiantes", { validator: { $jsonSchema: { /* ... */ } } });
-
-// Colección profesores (M2)
-db.createCollection("profesores", { validator: { $jsonSchema: { /* ... */ } } });
